f047c57a8cea2c255702d983aa6bfa8749f3bc1c
MEGHANA331 <53857407+MEGHANA331@users.noreply.github.com>
2020-11-05 12:02:36 +0530

    Update file_test.sh

--- Lab-3/file_test.sh
+++ Lab-3/file_test.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
-
-echo "Enter the 1st filename: "
-read filename1
-echo ""
-echo "Enter the 2nd filename: "
-read filename2
-if test -f $filename1 -a -f $filename2
+echo "Enter the file name"
+read file
+if test -f $file 
 then
-echo "The both the files called $filename1 and $filename2 are valid files(present in the system)"
-stat $filename1 $filename2
-elif test -f $filename1
-then
-echo "Only the first file called $filename1 is a valid file(present in the system)"
-stat $filename1
-elif test -f $filename2
-then
-echo "Only the second file called $filename2 is a valid file(present in the system)"
-stat $filename2
+echo "The entered $file is a file"
+echo $1 $2
+echo "The attributes of the file $file: "
+stat $file 
 else
-echo "The both entered files are invalid files they are not present in this system"
+echo "The entered $file is not a file or you have entered incorrect arguments"
 fi
-
