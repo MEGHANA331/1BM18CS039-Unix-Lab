9e1fd2e936e2ff587f9917a9afb26b04a1fc418c
MEGHANA331 <53857407+MEGHANA331@users.noreply.github.com>
2020-11-05 15:38:11 +0530

    Update Vowel-Constant.sh

--- Lab-6/Vowel-Constant.sh
+++ Lab-6/Vowel-Constant.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
-echo "Enter the character"
-read character
-if test $character = 'a' -o $character = 'e' -o $character = 'i' -o $character = 'u' -o $character = 'A' -o $character = 'E' -o $character = 'I' -o $character = 'O' -o $character = 'U'
-then
-echo "The entered character that is $character is a VOWEL"
-else
-echo "The entered character that is $character is a CONSTANT"
-fi
+echo "Enter the character:\c"
+read ch
+case $ch in
+	"a")echo "$ch is a vowel"
+	;;
+	"e")echo "$ch is a vowel"
+	;;
+	"i")echo "$ch is a vowel"
+	;;
+	"o")echo "$ch is a vowel"
+	;;
+	"u")echo "$ch is a vowel"
+ ;;
+ "A")echo "$ch is a vowel"
+	;;
+	"E")echo "$ch is a vowel"
+	;;
+	"I")echo "$ch is a vowel"
+	;;
+	"O")echo "$ch is a vowel"
+	;;
+	"U")echo "$ch is a vowel"
+	;;
+	*)echo "$ch is a consonent"
+esac
  
